337f4a1ec4061539dd2bf4ab1fcf6085d76acd1b
matheusgb <matheusgbr98@gmail.com>
2023-04-09 16:02:22 -0300

    fix: allows mathematical operations with float

--- basic_mathematics_bot.py
+++ basic_mathematics_bot.py
@@ -1,6 +1,7 @@
 import discord
 from decouple import config
 from discord.ext import commands
+from typing import Union
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,22 +14,27 @@ async def on_ready():
 
 
 @bot.command()
-async def add(ctx, num1: int, num2: int):
+async def easteregg(ctx):
+    await ctx.send("The most powerful enemy is yourself, that's the price of being aware of your own existence.")
+
+
+@bot.command()
+async def add(ctx, num1: Union[int, float], num2: Union[int, float]):
     await ctx.send(num1 + num2)
 
 
 @bot.command()
-async def subtract(ctx, num1: int, num2: int):
+async def subtract(ctx, num1: Union[int, float], num2: Union[int, float]):
     await ctx.send(num1 - num2)
 
 
 @bot.command()
-async def multiply(ctx, num1: int, num2: int):
+async def multiply(ctx, num1: Union[int, float], num2: Union[int, float]):
     await ctx.send(num1 * num2)
 
 
 @bot.command()
-async def divide(ctx, num1: int, num2: int):
+async def divide(ctx, num1: Union[int, float], num2: Union[int, float]):
     await ctx.send(num1 / num2)
 
 
